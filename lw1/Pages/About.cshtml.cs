ba3096de5f725e667369c0b95c43d0439070b4e0
hyth32 <5474n.sage@gmail.com>
2024-05-11 14:04:42 +0300

    lw1: removed unused imports

--- lw1/Pages/About.cshtml.cs
+++ lw1/Pages/About.cshtml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Valuator.Pages
 {
@@ -15,10 +9,6 @@ namespace Valuator.Pages
         public AboutModel(ILogger<AboutModel> logger)
         {
             _logger = logger;
-        }
-
-        public void OnGet()
-        {
         }
     }
 }
