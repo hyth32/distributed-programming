bf4acb60f71492ebc699e2764771e1f95a974b8f
hyth32 <5474n.sage@gmail.com>
2024-10-15 20:32:07 +0300

    добавил шестую лабораторную

---
+++ pa6/Chat/Client/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+class Program
+{
+    public static void StartClient(string hostaddress, int port, string msg)
+    {
+        try
+        {
+            // Разрешение сетевых имён
+            IPAddress ipAddress = (hostAddress.Equals("localhost", StringComparison.OrdinalIgnoreCase)) 
+                    ? IPAddress.Loopback 
+                    : IPAddress.Parse(hostAddress);
+
+            IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, port);
+
+            // CREATE
+            using (Socket clientSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    try
+                    {
+                        // Подключение
+                        clientSocket.Connect(remoteEndPoint);
+
+                        // Отправка сообщения
+                        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                        clientSocket.Send(messageBytes);
+
+                        // Прием ответа
+                        byte[] buffer = new byte[1024];
+                        int bytesReceived = clientSocket.Receive(buffer);
+                        string response = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+
+                        Console.WriteLine("{0}", response);
+
+                        // Завершение соединения
+                        clientSocket.Shutdown(SocketShutdown.Both);
+                    }
+                    catch (ArgumentNullException argEx)
+                    {
+                        Console.WriteLine("ArgumentNullException : {0}", argEx);
+                    }
+                    catch (SocketException socketEx)
+                    {
+                        Console.WriteLine("SocketException : {0}", socketEx);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Unexpected exception : {0}", ex);
+                    }
+                }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Invalid usage.\nUsage: dotnet run <hostAddress> <port> <msg>");
+            return;
+        }
+
+        string hostAddress = args[0];
+        bool isPortValid = int.TryParse(args[1], out int port);
+        string message = args[2];
+
+        if (String.IsNullOrEmpty(message))
+        {
+            Console.WriteLine("Empty message.");
+            return;
+        }
+
+        if (!isPortValid)
+        {
+            Console.WriteLine("Invalid port");
+            return;
+        }
+
+        StartClient(hostAddress, port, message);
+    }
+}
---
+++ pa6/Chat/Server/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Server;
+
+class Program
+{
+    public static void StartListening(int port)
+    {
+        // Привязываем сокет ко всем интерфейсам на текущей машинe
+        var ipAddress = IPAddress.Any;
+        var localEndPoint = new IPEndPoint(ipAddress, port);
+
+        // CREATE
+        using (Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+        {
+            try
+            {
+                // BIND
+                listener.Bind(localEndPoint);
+
+                // LISTEN
+                listener.Listen(10);
+
+                var messageHistory = new List<string>();
+
+                while (true)
+                {
+                    // ACCEPT
+                    using (Socket handler = listener.Accept())
+                    {
+                        byte[] buffer = new byte[1024];
+
+                        // RECEIVE
+                        int bytesReceived = handler.Receive(buffer);
+                        string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+
+                        Console.WriteLine("Message received: {0}", receivedData);
+                        messageHistory.Add(receivedData);
+
+                        // Объединяем все сообщения в одну строку
+                        string combinedMessages = string.Join(Environment.NewLine, messageHistory);
+
+                        // Преобразуем объединенную строку в массив байт
+                        byte[] responseMessage = Encoding.UTF8.GetBytes(combinedMessages);
+
+                        // SEND
+                        handler.Send(responseMessage);
+
+                        // RELEASE
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Invalid usage\nUsage: dotnet run <port>");
+            return;
+        }
+
+        bool isPortValid = int.TryParse(args[0], out int port);
+
+        if (!isPortValid)
+        {
+            Console.WriteLine("Invalid port");
+            return;
+        }
+
+        StartListening(port);
+    }
+}
